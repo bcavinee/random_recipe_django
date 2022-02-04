155d03a54286106ace1f6121efd07671874dd2ea
bcavinee <51139252+bcavinee@users.noreply.github.com>
2022-02-04 18:25:49 -0500

    d

--- recipescraper/settings.py
+++ recipescraper/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY= 'dc0e8b679af1571fbe2db2aef7182dfb98572dcdd376ee52'
+SECRET_KEY= os.environ.get("SECRET_KEY_SCRAPER") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG_VALUE'] == 'TRUE'
 
 ALLOWED_HOSTS = ['randomrecipegenerator.herokuapp.com']
-
+git 
 
 # Application definition
 
